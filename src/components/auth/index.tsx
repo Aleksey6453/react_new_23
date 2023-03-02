a06eb891ec39917089f7a78a82db9d3cecfe734c
Aleksey6453 <login6453@gmail.com>
2023-03-02 03:18:05 +0200

    Comit about finish login and register

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -2,10 +2,33 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import LoginPage from './login/loginPage'
 import RegisterPage from './register/register'
+import './style.css'
+import { Box } from '@mui/material';
 
 const AuthRootComponent = () => {
   const location = useLocation()
-  return (location.pathname === '/login' ? <LoginPage /> : location.pathname === '/register' ? <RegisterPage /> : null) 
+  return(
+    <div className='root'>
+      <div className="form">
+        <Box 
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          flexDirection='column'
+          maxWidth={640}
+          margin='auto'
+          padding={4}
+          borderRadius={5}
+          bgcolor='#F0F7FF'
+          color='#A0A0A0'
+          border='2px solid #A0A0A0'
+        >
+          {location.pathname === '/login' ? <LoginPage /> : location.pathname === '/register' ? <RegisterPage /> : null}
+        </Box>
+      </div>
+    </div>
+  )
+  
 }
 
 export default AuthRootComponent
--- src/components/auth/login/loginPage.tsx
+++ src/components/auth/login/loginPage.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import Test from '../../Test'
-import styles from './style.module.css'
+import {TextField, Button, Typography} from '@mui/material'
 
 const LoginPage = () => {
-  const name = "Alexandro"
-  const gnom = "Genomio"
+
   
   return (
-    <div>
-      <h1 className={styles.color}>Fucking login Page</h1>
-      <Test gnom = {gnom}/>
-    </div>
+    <>
+      <Typography variant="h3">
+        Authorization
+      </Typography>
+      <Typography variant="body1" marginBottom={2}>
+        Enter email and password please
+      </Typography>
+      <TextField fullWidth={true} sx={{marginTop:'10px'}} label="Email" variant="outlined" placeholder='Enter your email'/>
+      <TextField type='password' fullWidth={true} sx={{marginTop:'10px'}} label="Password" variant="outlined" placeholder='Enter your password'/>
+      <Button sx={{marginTop:'15px', width: '50%', padding: '15px'}} variant="contained">Enter</Button>
+      <Typography variant="body1" sx={{marginTop:'10px'}}>
+       Dont have an account? <span className='incitingText'>Registrations!</span>
+      </Typography>
+    </>
   )
 }
 
