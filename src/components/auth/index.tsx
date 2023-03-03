98cd49c578bd9b9cbe76b67cf327254c497a1f37
Aleksey6453 <login6453@gmail.com>
2023-03-03 18:47:11 +0200

    03.03.23

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import LoginPage from './login/loginPage'
 import RegisterPage from './register/register'
@@ -6,10 +6,17 @@ import './style.css'
 import { Box } from '@mui/material';
 
 const AuthRootComponent = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const location = useLocation()
+
+  const handleSubmit = async (e: {preventDefault:()=>void}) => {
+      e.preventDefault()
+  } 
+
   return(
     <div className='root'>
-      <div className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <Box 
           display='flex'
           justifyContent='center'
@@ -23,9 +30,9 @@ const AuthRootComponent = () => {
           color='#A0A0A0'
           border='2px solid #A0A0A0'
         >
-          {location.pathname === '/login' ? <LoginPage /> : location.pathname === '/register' ? <RegisterPage /> : null}
+          {location.pathname === '/login' ? <LoginPage setEmail={setEmail} setPassword={setPassword} /> : location.pathname === '/register' ? <RegisterPage /> : null}
         </Box>
-      </div>
+      </form>
     </div>
   )
   
--- src/components/auth/login/loginPage.tsx
+++ src/components/auth/login/loginPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {TextField, Button, Typography} from '@mui/material'
 
-const LoginPage = () => {
+const LoginPage = (props: any) => {
 
+  const {setPassword, setEmail} = props
   
   return (
     <>
@@ -12,9 +13,9 @@ const LoginPage = () => {
       <Typography variant="body1" marginBottom={2}>
         Enter email and password please
       </Typography>
-      <TextField fullWidth={true} sx={{marginTop:'10px'}} label="Email" variant="outlined" placeholder='Enter your email'/>
-      <TextField type='password' fullWidth={true} sx={{marginTop:'10px'}} label="Password" variant="outlined" placeholder='Enter your password'/>
-      <Button sx={{marginTop:'15px', width: '50%', padding: '15px'}} variant="contained">Enter</Button>
+      <TextField fullWidth={true} onChange={(e)=>{setEmail(e.target.value)}} sx={{marginTop:'10px'}} label="Email" variant="outlined" placeholder='Enter your email'/>
+      <TextField type='password' onChange={(e)=>{setPassword(e.target.value)}} fullWidth={true} sx={{marginTop:'10px'}} label="Password" variant="outlined" placeholder='Enter your password'/>
+      <Button type='submit' sx={{marginTop:'15px', width: '50%', padding: '15px'}} variant="contained">Enter</Button>
       <Typography variant="body1" sx={{marginTop:'10px'}}>
        Dont have an account? <span className='incitingText'>Registrations!</span>
       </Typography>
